4ef91a383621b91d509509c50f09c2e10644d456
Francis Bourre <francis.bourre@luxoffice.duodecadits.com>
2015-12-01 11:23:43 +0100

    Flash CI

--- test/MainMVCTest.hx
+++ test/MainMVCTest.hx
@@ -2,6 +2,7 @@ package;
 
 import hex.HexMVCSuite;
 import hex.unittest.notifier.ConsoleNotifier;
+import hex.unittest.notifier.TraceNotifier;
 import hex.unittest.runner.ExMachinaUnitCore;
 
 /**
@@ -13,8 +14,15 @@ class MainMVCTest
 	static public function main() : Void
 	{
 		var emu : ExMachinaUnitCore = new ExMachinaUnitCore();
-        emu.addListener( new ConsoleNotifier( false ) );
-        emu.addTest( hex.HexMVCSuite );
+        
+		#if flash
+		TestRunner.RENDER_DELAY = 0;
+		emu.addListener( new TraceNotifier( false ) );
+		#else
+		emu.addListener( new ConsoleNotifier( false ) );
+		#end
+		
+        emu.addTest( HexMVCSuite );
         emu.run();
 	}
 }
