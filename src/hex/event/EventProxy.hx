87b1b8017383d4a25565deffaa5f6b8027b7de11
Francis Bourre <francis.bourre@luxoffice.duodecadits.com>
2015-12-11 09:25:38 +0100

    EventProxy class added

--- src/hex/event/EventProxy.hx
+++ src/hex/event/EventProxy.hx
@@ -8,13 +8,30 @@ import hex.event.IEvent;
  */
 class EventProxy implements IEventListener
 {
-	public function new() 
+	public var target 						: Dynamic;
+	public var method 						: Dynamic;
+	public var appendEventToArguments 		: Bool;
+	public var arguments 					: Array<Dynamic>;
+	
+	public function new( ?target : Dynamic, ?method : Dynamic, ?appendEventToArguments : Bool = true, arguments : Array<Dynamic> ) 
 	{
-		
+		this.target 				= target;
+		this.method 				= method;
+		this.arguments 				= arguments;
 	}
 
 	public function handleEvent( e : IEvent ) : Void 
 	{
-		
+		if ( this.target != null && this.method != null )
+		{
+			if ( this.appendEventToArguments )
+			{
+				Reflect.callMethod( this.target, this.method, this.arguments.concat( [e] ) );
+			}
+			else
+			{
+				Reflect.callMethod( this.target, this.method, this.arguments );
+			}
+		}
 	}
 }
