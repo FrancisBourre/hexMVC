306b91646455330b6906c9748f4eb07365c54e91
grosmar <zubak.balazs@gmail.com>
2016-02-03 17:07:46 +0100

    Base model implementation for managing dispatcher

---
+++ src/hex/model/IModelRO.hx
@@ -0,0 +1,15 @@
+package hex.model;
+import hex.model.IModelListener;
+/**
+ * @author duke
+ */
+
+interface IModelRO<ListenerType:IModelListener>
+{
+	
+	function addListener(listener:ListenerType):Void;
+	
+	function removeListener(listener:ListenerType):Void;
+	
+	
+}
