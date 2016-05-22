0d0a0f10e6e042c021c9b1e1ad656afa552498c3
Francis Bourre <francis_bourre@me.com>
2016-05-22 07:25:41 +0200

    BasicView class added for fast view implementation

---
+++ src/hex/view/BasicView.hx
@@ -0,0 +1,25 @@
+package hex.view;
+
+/**
+ * ...
+ * @author Francis Bourre
+ */
+class BasicView implements IView
+{
+	public function new() 
+	{
+		
+	}
+	
+	@:isVar public var visible( get, set ) : Bool;
+	
+	function get_visible() : Bool 
+	{
+		return visible;
+	}
+	
+	function set_visible( value : Bool ) : Bool 
+	{
+		return visible = value;
+	}
+}
